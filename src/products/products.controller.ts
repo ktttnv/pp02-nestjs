6f06189ef2e492227955c70131c5a67269e23556
Ekaterina Tatanova <e.tatanova@mail.ru>
2021-01-27 23:10:10 +0300

    Added model for schema

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Delete, Get, Header, HttpCode, HttpStatus, Param, Pos
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { ProductsService } from './products.service';
+import { Product } from './schemas/product.schema';
 
 @Controller('products')
 export class ProductsController {
@@ -10,30 +11,29 @@ export class ProductsController {
     }
 
     @Get()
-    // @Redirect('https://google.com', 301)
-    getAll() {
+    getAll(): Promise<Product[]> {
         return this.productsService.getAll();
     }
 
     @Get(':id')
-    getOne(@Param('id') id: string) {
+    getOne(@Param('id') id: string): Promise<Product> {
         return this.productsService.getById(id);
     }
 
     @Post()
     @HttpCode(HttpStatus.CREATED)
     @Header('Cache-Control', 'none')
-    create(@Body() createProductDto: CreateProductDto) {
+    create(@Body() createProductDto: CreateProductDto): Promise<Product> {
         return this.productsService.create(createProductDto);
     }
 
     @Delete(':id')
-    remove(@Param('id') id: string) {
-        return 'Remove ' + id;
+    remove(@Param('id') id: string): Promise<Product> {
+        return this.productsService.remove(id);
     }
 
     @Put(':id')
-    update(@Body() updateProductDto: UpdateProductDto, @Param('id') id: string) {
-        return 'Update ' + id;
+    update(@Body() updateProductDto: UpdateProductDto, @Param('id') id: string): Promise<Product> {
+        return this.productsService.update(id, updateProductDto);
     }
 }
